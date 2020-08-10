6031af83b41df2766a0cf2709f246ccb4b0044f1
Obe <o.bdondo@gmail.com>
2020-08-09 22:01:48 -0700

    Added DB connection string

--- eCommerceSite/Startup.cs
+++ eCommerceSite/Startup.cs
@@ -30,6 +30,13 @@ namespace eCommerceSite
 
             services.AddDbContext<ProductContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
+        //Alternative format for code above
+        //
+        //private void AddSqlServer(DbContextOptionsBuilder options)
+        //{
+        //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnect"))
+        //
+        //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
