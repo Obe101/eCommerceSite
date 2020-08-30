303e70d027a1be946b022624e2b28eea2f923f72
Obe <o.bdondo@gmail.com>
2020-08-29 18:19:03 -0700

    Added login check for bad credentials

--- eCommerceSite/Controllers/UserController.cs
+++ eCommerceSite/Controllers/UserController.cs
@@ -51,19 +51,33 @@ namespace eCommerceSite.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
             UserAccount account = 
                               await (from u in _context.UserAccounts
                                    where (u.Username == model.UsernameOrEmail ||
                                         u.Email == model.UsernameOrEmail) &&
                                         u.Password == model.Password
                                    select u).SingleOrDefaultAsync();
+            //Method syntax 
+            //UserAccount account = 
+            //        _context.UserAccounts
+            //                .Where(userAcc => userAcc.UserName == model.UsernameOrEmail ||
+            //                                   userAcc.email == model.UsernameOrEmail) &&
+            //                                   UserAcc.Password == model.Password
+            //                                   ).SingleOrDefaultAsync();
             if(account == null)
             {
                 // Credentials did not match
+
+                // Custom error message
+                ModelState.AddModelError(string.Empty, "Credentials were not found");
                 return View(model);
             }
             //log user into website
-            return RedirectToAction("Index", "Home")
+            return RedirectToAction("Index", "Home");
         }
     }
 }
