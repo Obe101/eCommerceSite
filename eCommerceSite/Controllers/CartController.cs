3458b6c950a3654e587c7e3e013f67f3049b726f
Obe <o.bdondo@gmail.com>
2020-08-30 21:55:24 -0700

    Reefractored get product db query

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCommerceSite.Data;
+using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Add(int id) // Id of the produuct to add
+        private readonly ProductContext _context;
+        public CartController(ProductContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// /Adds product to shopping cart
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Add(int id) // Id of the produuct to add
         {
             //Get product from the database
+            Product p = await (from products in _context.Products
+                               where products.ProductId == id
+                               select products).SingleAsync();
             //Add product to cart cookies
 
             // Redirectback to previous page
