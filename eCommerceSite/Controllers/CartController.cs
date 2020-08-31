e36b7bc4b6a992da7b27a171f5349bc4ba55be34
Obe <o.bdondo@gmail.com>
2020-08-30 23:30:56 -0700

    Refractored cart controller

--- eCommerceSite/Controllers/CartController.cs
+++ eCommerceSite/Controllers/CartController.cs
@@ -29,28 +29,10 @@ namespace eCommerceSite.Controllers
         {
             //Get product from the database
             Product p = await ProductDb.GetProductAsync(_context, id);
-            const string CartCookie = "CartCookie";
+
+            CookieHelper.AddProductToCart(_httpContext, p);
+
             
-            //Get existing cart items
-            string exisingCart =_httpContext.HttpContext.Request.Cookies[CartCookie];
-            List<Product> cartProducts = new List<Product>();
-            if(exisingCart != null)
-            {
-                 cartProducts = JsonConvert.DeserializeObject<List <Product>>(exisingCart);
-            }
-
-            //Add current products to existing cart
-            cartProducts.Add(p);
-
-            //Add product to cart cookies
-            string data = JsonConvert.SerializeObject(p);
-            CookieOptions options = new CookieOptions()
-            {
-                Expires = DateTime.Now.AddYears(1),
-                Secure = true,
-                IsEssential = true,
-            };
-            _httpContext.HttpContext.Response.Cookies.Append(CartCookie, data, options);
             // Redirectback to previous page
             return RedirectToAction("Index", "Product");
         }
@@ -60,8 +42,8 @@ namespace eCommerceSite.Controllers
             //Display all products in shopping cart cookie
             string cookieData = _httpContext.HttpContext.Request.Cookies["CartCookie"];
 
-            List<Product> cartProducts = JsonConvert.DeserializeObject<List<Product>>(cookieData);
-            return View();
+            List<Product> cartProducts = CookieHelper.GetCartProducts(_httpContext);
+            return View(cartProducts);
         }
     }
 }
